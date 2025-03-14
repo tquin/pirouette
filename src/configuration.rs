0431f46f6805074f89acbc946ff60772872a0741
tquin <49335547+tquin@users.noreply.github.com>
2025-03-14 21:29:44 +1100

    wip: messy retention format conversion

--- src/configuration.rs
+++ src/configuration.rs
@@ -7,26 +7,54 @@ use anyhow::{Context, Result};
 use anyhow;
 
 #[derive(Debug, Deserialize)]
+pub struct PirouetteConfigRaw {
+    pub source: ConfigPath,
+    pub target: ConfigPath,
+    pub retention: ConfigRetentionRaw,
+}
+
+#[derive(Debug, Deserialize)]
 pub struct PirouetteConfig {
-    source: ConfigPath,
-    target: ConfigPath,
-    retention: ConfigRetention,
+    pub source: ConfigPath,
+    pub target: ConfigPath,
+    pub retention: Vec<ConfigRetention>,
 }
 
 #[derive(Debug, Deserialize)]
 pub struct ConfigPath {
-    path: path::PathBuf,
+    pub path: path::PathBuf,
 }
 
 #[derive(Debug, Deserialize)]
 #[allow(unused)]
-pub struct ConfigRetention {
+pub struct ConfigRetentionRaw {
     hours: Option<u32>,
     days: Option<u32>,
     weeks: Option<u32>,
     months: Option<u32>,
     years: Option<u32>,
 }
+
+#[derive(Debug, Deserialize)]
+#[allow(unused)]
+pub struct ConfigRetention {
+    kind: ConfigRetentionKind,
+    value: u32,
+}
+
+#[derive(Debug, Deserialize)]
+#[allow(unused)]
+pub enum ConfigRetentionKind {
+    Hours,
+    Days,
+    Weeks,
+    Months,
+    Years,
+}
+
+/*
+    Read config from disk
+*/
 
 fn get_config_file_path() -> path::PathBuf {
     let config_file_path = match env::var("PIROUETTE_CONFIG_FILE") {
@@ -49,9 +77,12 @@ fn get_config_file_path_default() -> path::PathBuf {
     return config_file_path;
 }
 
-fn validate_config_source(source: &ConfigPath) -> Result<()> {
-    // A valid `source` can be any file or directory
+/*
+    User input validation
+*/
 
+// A valid `source` can be any file or directory
+fn validate_config_source(source: &ConfigPath) -> Result<()> {
     // Bad input: source doesn't exist
     if !source.path.exists() {
         anyhow::bail!("source path does not exist");
@@ -60,10 +91,9 @@ fn validate_config_source(source: &ConfigPath) -> Result<()> {
     Ok(())
 }
 
+// A valid `target` is only a directory
 fn validate_config_target(target: &ConfigPath) -> Result<()> {
-    // A valid `target` is only a directory
-
-    // Path doesn't already exist, but we can create it ourselves
+    // Path doesn't already exist, but we can try to create it ourselves
     if !target.path.exists() {
         fs::create_dir_all(&target.path)
             .context("failed to create target directory")?;
@@ -77,34 +107,92 @@ fn validate_config_target(target: &ConfigPath) -> Result<()> {
     Ok(())
 }
 
-impl ConfigRetention {
+impl ConfigRetentionRaw {
+    fn retention_field_list(&self) -> Vec<Option<u32>> {
+        return vec![self.hours, self.days, self.weeks, self.months, self.years];
+    }
+
     fn at_least_one_populated_field(&self) -> bool {
-        let fields = [&self.hours, &self.days, &self.weeks, &self.months, &self.years];
-        fields.iter().any(|&field| field.is_some())
+        self.retention_field_list().into_iter()
+            .any(|field| field.is_some())
     }
 }
 
-#[allow(unused)]
-fn validate_config_retention(retention: &ConfigRetention) -> Result<()> {
-    // A valid `retention` has at least one non-None field
+// A valid `retention` has at least one non-None field
+fn validate_config_retention(retention: &ConfigRetentionRaw) -> Result<()> {
     match &retention.at_least_one_populated_field() {
         true => (),
+        // Bad input: no retention values provided
         false => anyhow::bail!("no retention period was specified"),
     }
 
     Ok(())
 }
 
-fn validate_config(config_file_toml: &PirouetteConfig) -> Result<()> {
-    // These may panic on bad user input
-    validate_config_source(&config_file_toml.source)
+fn check_user_input(raw_config: &PirouetteConfigRaw) -> Result<()> {
+    validate_config_source(&raw_config.source)
         .context("failed to validate source")?;
-    validate_config_target(&config_file_toml.target)
+    validate_config_target(&raw_config.target)
         .context("failed to validate target")?;
-    validate_config_retention(&config_file_toml.retention)
+    validate_config_retention(&raw_config.retention)
         .context("failed to validate retention")?;
 
     Ok(())
+}
+
+/*
+    Data conversion
+*/
+
+fn convert_retention_type(raw_retention: &ConfigRetentionRaw) -> Result<Vec<ConfigRetention>> {
+    let mut converted_retention = vec![];
+
+    if raw_retention.hours.is_some() {
+        converted_retention.push(
+            ConfigRetention {
+                kind: ConfigRetentionKind::Hours,
+                value: raw_retention.hours.unwrap(),
+            }
+        )
+    }
+
+    if raw_retention.days.is_some() {
+        converted_retention.push(
+            ConfigRetention {
+                kind: ConfigRetentionKind::Days,
+                value: raw_retention.days.unwrap(),
+            }
+        )
+    }
+
+    if raw_retention.weeks.is_some() {
+        converted_retention.push(
+            ConfigRetention {
+                kind: ConfigRetentionKind::Weeks,
+                value: raw_retention.weeks.unwrap(),
+            }
+        )
+    }
+
+    if raw_retention.months.is_some() {
+        converted_retention.push(
+            ConfigRetention {
+                kind: ConfigRetentionKind::Months,
+                value: raw_retention.months.unwrap(),
+            }
+        )
+    }
+
+    if raw_retention.years.is_some() {
+        converted_retention.push(
+            ConfigRetention {
+                kind: ConfigRetentionKind::Years,
+                value: raw_retention.years.unwrap(),
+            }
+        )
+    }
+
+    Ok(converted_retention)
 }
 
 pub fn parse_config() -> Result<PirouetteConfig> {
@@ -114,10 +202,16 @@ pub fn parse_config() -> Result<PirouetteConfig> {
         .with_context(|| format!("failed to read config file: {}", config_file_path.display()))?;
 
     // Parse the toml into a struct
-    let config_file_toml: PirouetteConfig = toml::from_str(&config_file_str)
+    let raw_config: PirouetteConfigRaw = toml::from_str(&config_file_str)
         .with_context(|| format!("failed to parse config file: {}", config_file_path.display()))?;
 
-    // Validate the format, create paths if required, etc.
-    validate_config(&config_file_toml)?;
-    Ok(config_file_toml)
+    // Panic if we have any invalid input
+    check_user_input(&raw_config)?;
+
+    // Convert raw format into type-compliant data
+    Ok(PirouetteConfig {
+        source: raw_config.source,
+        target: raw_config.target,
+        retention: convert_retention_type(&raw_config.retention)?,
+    })
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
 use anyhow::{Result};
 
+use crate::configuration::PirouetteConfig;
 mod configuration;
 
 fn main() -> Result<()> {
-    let config = configuration::parse_config();
+    let config = configuration::parse_config()?;
     println!("config {:#?}", config);
 
+    check_target_state(&config);
+
     Ok(())
+}
+
+fn check_target_state(_config: &PirouetteConfig) {
+    // let x = config.retention.all_retention_fields().into_iter();
+    // println!("{:#?}", x);
 }
